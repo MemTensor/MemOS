e098305fb27d2547c1f8ab9ca9a947877639d3ca
CSLiuPeng <liu_peng_email@163.com>
2025-08-01 16:25:20 +0800

    update locomo rag bash file

--- evaluation/scripts/run_rag_eval.sh
+++ evaluation/scripts/run_rag_eval.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-LIB="mem0"
+LIB="rag"
 VERSION="default"
+DATA_SET="locomo"
+CHUNK_SIZE=128
+NUM_CHUNKS=1
+export HF_ENDPOINT=https://hf-mirror.com
+mkdir -p results/$DATA_SET/$LIB-$VERSION/
+echo "The result saved in：results/$DATA_SET/$LIB-$VERSION/"
 
 echo "The complete evaluation steps for generating the RAG and full context!"
 
 echo "Running locomo_rag.py..."
-python scripts/locomo/locomo_rag.py --chunk_size 512 --num_chunks 1 --output_folder results/locomo/mem0-default/
+python scripts/locomo/locomo_rag.py \
+    --chunk_size $CHUNK_SIZE \
+    --num_chunks $NUM_CHUNKS \
+    --frame $LIB \
+    --output_folder "results/$DATA_SET/$LIB-$VERSION/"
+
 if [ $? -ne 0 ]; then
     echo "Error running locomo_rag.py"
     exit 1
@@ -27,3 +38,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 echo "✅✅✅Evaluation score have been generated!"
+
+echo "Save the experimental results of this round..."
+DIR="results/$DATA_SET/"
+cd "$DIR" || { echo "Unable to enter directory $DIR"; exit 1; }
+
+# Rename the folder to avoid being overwritten by new results
+OLD_NAME="$LIB-$VERSION"
+NEW_NAME="$LIB-$CHUNK_SIZE-$NUM_CHUNKS"
+
+if [ -d "$OLD_NAME" ]; then
+    # Rename the folder
+    mv "$OLD_NAME" "$NEW_NAME"
+
+    # Output prompt information
+    echo "Already rename the folder: $OLD_NAME → $NEW_NAME"
+else
+    echo "Error:Folder $OLD_NAME is not exist"
+    exit 1
+fi
+echo "✅✅✅✅ All the experiment has been successful..."
