415fafdeadd71dea9bb6c6878024255309654c36
chentang <travistang@foxmail.com>
2025-08-06 19:30:01 +0800

    fix bugs: modify configs, examples, schedule handlers of mem_scheduler, and rename some variables

--- src/memos/mem_os/product.py
+++ src/memos/mem_os/product.py
@@ -491,7 +491,7 @@ class MOSProduct(MOSCore):
                 mem_cube=self.mem_cubes[mem_cube_id],
                 label=label,
                 content=query,
-                timestamp=datetime.now(),
+                timestamp=datetime.now().isoformat(),
             )
             self.mem_scheduler.submit_messages(messages=[message_item])
 
