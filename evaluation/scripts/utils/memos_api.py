fa9da463bbc4dc885817b6d721e5c980bf7b5145
Hao <120852460@qq.com>
2025-09-11 14:57:29 +0800

    mirix funcs

--- evaluation/scripts/utils/memos_api.py
+++ evaluation/scripts/utils/memos_api.py
@@ -1,37 +1,36 @@
 import json
-
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+memos_key = os.getenv("MEMOS_KEY")
+memos_url = os.getenv("MEMOS_URL")
 
 
 class MemOSAPI:
-    def __init__(self, base_url: str = "http://localhost:8000"):
+    def __init__(self, base_url: str = memos_url, memos_key: str = memos_key):
         self.base_url = base_url
-        self.headers = {"Content-Type": "application/json"}
+        self.headers = {"Content-Type": "application/json", "Authorization": memos_key}
 
-    def user_register(self, user_id: str):
-        """Register a user."""
-        url = f"{self.base_url}/users/register"
-        payload = json.dumps({"user_id": user_id})
-        response = requests.request("POST", url, data=payload, headers=self.headers)
-        return response.text
-
-    def add(self, messages: list[dict], user_id: str | None = None):
+    def add(self, messages: list[dict], user_id: str | None = None, conv_id: str | None = None):
         """Create memories."""
-        register_res = json.loads(self.user_register(user_id))
-        cube_id = register_res["data"]["mem_cube_id"]
-        url = f"{self.base_url}/add"
-        payload = json.dumps({"messages": messages, "user_id": user_id, "mem_cube_id": cube_id})
 
+        url = f"{self.base_url}/add/message"
+        payload = json.dumps({"messages": messages, "userId": user_id, "conversationId": conv_id})
         response = requests.request("POST", url, data=payload, headers=self.headers)
         return response.text
 
-    def search(self, query: str, user_id: str | None = None, top_k: int = 10):
+    def search(self, query: str, user_id: str | None = None, conv_id: str | None = '', top_k: int = 10):
         """Search memories."""
-        url = f"{self.base_url}/search"
+        url = f"{self.base_url}/search/memory"
         payload = json.dumps(
             {
                 "query": query,
-                "user_id": user_id,
+                "userId": user_id,
+                "conversationId": conv_id,
+                "memoryLimitNumber": top_k
             }
         )
 
@@ -39,25 +38,22 @@ class MemOSAPI:
         if response.status_code != 200:
             response.raise_for_status()
         else:
-            result = json.loads(response.text)["data"]["text_mem"][0]["memories"]
-            text_memories = [item["memory"] for item in result][:top_k]
-            return text_memories
+            return json.loads(response.text)["data"]
 
 
 if __name__ == "__main__":
-    client = MemOSAPI(base_url="http://localhost:8000")
-    # Example usage
-    try:
-        messages = [
-            {
-                "role": "user",
-                "content": "I went to the store and bought a red apple.",
-                "chat_time": "2023-10-01T12:00:00Z",
-            }
+    client = MemOSAPI()
+    user_id = 'eval_test'
+    conv_id = 'eval_test_benchmark1_conv1'
+    messages = [
+            {"role": "user", "content": "杭州西湖有什么好玩的"},
+            {"role": "assistant", "content": "杭州西湖有好多松鼠，还有断桥"}
         ]
-        add_response = client.add(messages, user_id="user789")
-        print("Add memory response:", add_response)
-        search_response = client.search("red apple", user_id="user789", top_k=1)
-        print("Search memory response:", search_response)
-    except requests.RequestException as e:
-        print("An error occurred:", e)
+
+    memories = client.search("我最近有什么记忆", user_id=user_id, top_k=6)
+    response = client.add(messages, user_id, conv_id)
+    memories = client.search("我最近有什么记忆", user_id=user_id, top_k=6)
+
+
+
+
