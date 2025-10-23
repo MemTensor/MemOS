c27bd61af8cace0b4ae550ec473c1e7b99c83038
harvey_xiang <harvey_xiang22@163.com>
2025-10-23 17:59:27 +0800

    feat: add error log

--- src/memos/log.py
+++ src/memos/log.py
@@ -171,10 +171,10 @@ LOGGING_CONFIG = {
     "disable_existing_loggers": False,
     "formatters": {
         "standard": {
-            "format": "%(asctime)s [%(trace_id)s] - %(env)s | %(user_type)s | %(user_name)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s"
+            "format": "%(asctime)s | %(trace_id)s | %(env)s | %(user_type)s | %(user_name)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s"
         },
         "no_datetime": {
-            "format": "[%(trace_id)s] - %(env)s | %(user_type)s | %(user_name)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s"
+            "format": "%(trace_id)s| %(env)s | %(user_type)s | %(user_name)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s"
         },
         "simplified": {
             "format": "%(asctime)s | %(trace_id)s | %(env)s | %(user_type)s | %(user_name)s | %(levelname)s | %(filename)s:%(lineno)d: %(funcName)s | %(message)s"
