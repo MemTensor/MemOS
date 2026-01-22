1663831a1565dfff948df8093271bf630a0e07d9
glin1993@outlook.com <>
2026-01-22 21:06:46 +0800

    chore: format advanced_searcher

--- src/memos/memories/textual/tree_text_memory/retrieve/advanced_searcher.py
+++ src/memos/memories/textual/tree_text_memory/retrieve/advanced_searcher.py
@@ -55,9 +55,7 @@ class AdvancedSearcher(Searcher):
 
         self.stage_retrieve_top = 3
         if process_llm is None:
-            logger.warning(
-                "[AdvancedSearcher] process_llm is None, falling back to dispatcher_llm"
-            )
+            logger.warning("[AdvancedSearcher] process_llm is None, falling back to dispatcher_llm")
         self.process_llm = process_llm or dispatcher_llm
         self.thinking_stages = 3
         self.max_retry_times = 2
