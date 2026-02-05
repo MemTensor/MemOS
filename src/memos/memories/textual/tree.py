08d4f44c66645d2f71b04151fa0bbc75558a3222
HarveyXiang <harvey_xiang@163.com>
2026-02-05 16:27:41 +0800

    Revert "fix: The "total nodes" information returned by the "get memory" interface is inaccurate. " (#1021)
    Revert "fix: The "total nodes" information returned by the "get memory" inter…"
    
    This reverts commit da91ffc8ad807ae22210cd365096ffc2272047a3.

--- src/memos/memories/textual/tree.py
+++ src/memos/memories/textual/tree.py
@@ -364,19 +364,13 @@ class TreeTextMemory(BaseTextMemory):
         page: int | None = None,
         page_size: int | None = None,
         filter: dict | None = None,
-        memory_type: list[str] | None = None,
     ) -> dict:
         """Get all memories.
         Returns:
             list[TextualMemoryItem]: List of all memories.
         """
         graph_output = self.graph_store.export_graph(
-            user_name=user_name,
-            user_id=user_id,
-            page=page,
-            page_size=page_size,
-            filter=filter,
-            memory_type=memory_type,
+            user_name=user_name, user_id=user_id, page=page, page_size=page_size, filter=filter
         )
         return graph_output
 
